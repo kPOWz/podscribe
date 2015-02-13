cc6de9235bcea2cb66baeeae6e647f35b7b0a3e9
kPOWz <karrie.zeman@gmail.com>
2015-02-13 16:20:26 -0600

    composer update

--- makerecording.php
+++ makerecording.php
@@ -8,7 +8,7 @@
     echo('<?xml version="1.0" encoding="UTF-8"?>');
 ?>
 <Response>
-    <Say>Hello. This is a call from the transcribe.me podcast transcription service.
+    <Say>Hello. This is a call from the podscribe.me podcast transcription service.
 Please start casting after the beep, and remember to speak clearly.</Say>
     <Record transcribe="true" transcribeCallback="<?php
         echo "transcribed.php?email=".urlencode($_REQUEST['email']); ?>"
